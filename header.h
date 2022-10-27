d408091a6cbd88f1179147bb63ca5010becbfbce
FERNANDA SUOMINSKY <82189986+Ferskys@users.noreply.github.com>
2022-10-27 19:18:12 -0300

    testes
    bora

---
+++ header.h
@@ -0,0 +1,11 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_printf(const char *format, ...);
+
+#endif
