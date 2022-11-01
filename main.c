a72d0d5dd9a917b864b61df6040eb858234ccaba
FERNANDA SUOMINSKY <82189986+Ferskys@users.noreply.github.com>
2022-11-01 17:11:05 -0300

    corrigido

--- main.c
+++ main.c
@@ -5,7 +5,8 @@ int	main(void)
 	unsigned int	j = 1;
 	long	int	x;
 	int c;
-	unsigned long	k;
+	int c1;
+	unsigned long	*k;
 
 	// printf("\nPRINTF ORIGINAL MIN-LONG in POINTER: %p\n", −2147483648);
 	// ft_printf("FT_PRINTF ORIGINAL MIN-LONG in POINTER: %p\n\n", −2147483648);
@@ -14,7 +15,35 @@ int	main(void)
 	// c = ft_printf("\n\n > TESTES CHAR: |%c|-|%c|-|%c| \n\n", 'x', 0, 'z');
 	// printf("A printf retornou: %i\n\n", c);
 
-	// k = -2147483648;
+	k = -2147483648;
+
+	printf("\n\n %x \n\n", 9223372036854775807LL);
+	ft_printf("\n\n %x \n\n", 9223372036854775807LL);
+
+	c1 = ft_printf("\n POINTER: %p", &k);
+	c = printf("\n POINTER: %p", &k);
+
+	printf("\n Resultados :%i, %i ", c1, c);
+
+	c1 = printf("\n 100 :%i ", 100);
+	c = ft_printf("\n 100 :%i ", 100);
+
+	printf("\n Resultados :%i, %i ", c1, c);
+
+	c1 = printf("\n -100 :%i ", -100);
+	c = ft_printf("\n -100 :%i ", -100);
+	
+	printf("\n Resultados :%i, %i ", c1, c);
+
+	c1 = printf("\n MAX :%i ", 2147483647);
+	c = ft_printf("\n MAX :%i ", 2147483647);
+
+	printf("\n Resultados :%i, %i ", c1, c);
+
+	c1 = printf("\n MIN :%i ", -2147483648);
+	c = ft_printf("\n MIN :%i ", -2147483648);
+
+	printf("\n Resultados :%i, %i ", c1, c);
 
 	// printf("\n\n TESTE UNSIGNED LONG %lu", k);
 	// c = printf("\n\n > TESTES ZEROX: |%X| \n", 0);
@@ -39,6 +68,8 @@ int	main(void)
 	// printf("RESULTADO DO PRINTF: %i\n\n", c);
 
 	printf("\nPRINTF ORIGINAL -->\n");
+	c = ft_printf(" > STRING = %s\n > PORCENTAGEM = %%\n > INTEIRO = %i\n > CHAR = %c\n > BASE 10 = %d\n > UNSIGNED BASE 10 = %u\n > PONTEIRO = %p\n > HEX MINUSCULO = %x\n > HEX MAIUSCULO = %X\n", "Hello world!", 123, 'm', 4321, 121, &j, 3718371, 3718371);
+	printf("A ft_printf retornou: %i\n\n", c);
 	c = printf(" > STRING = %s\n > PORCENTAGEM = %%\n > INTEIRO = %i\n > CHAR = %c\n > BASE 10 = %d\n > UNSIGNED BASE 10 = %u\n > PONTEIRO = %p\n > HEX MINUSCULO = %x\n > HEX MAIUSCULO = %X\n", "Hello world!", 123, 'm', 4321, 121, &j, 3718371, 3718371);
 	printf("A printf retornou: %i\n\n", c);
 	ft_printf("PRINTF HOMEBREW -->\n");
